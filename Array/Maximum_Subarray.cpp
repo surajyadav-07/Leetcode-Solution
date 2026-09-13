3f73ccbbcea9287edcfbbe63e6f751abf5679691
surajyadav-07 <surajyadav63072@gmail.com>
2026-09-13 12:55:46 +0530

    Added Maximum Subarray Solution

---
+++ Array/Maximum_Subarray.cpp
@@ -0,0 +1,20 @@
+// Leetcode 53 - Maximum Subarray
+//Kadane's Algorithm
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currsum=0 , maxsum = INT_MIN;
+        for (int val :nums){
+        currsum += val;
+        maxsum = max(currsum,maxsum);
+        if (currsum<0){
+            currsum = 0;
+        }
+        
+        }
+    return maxsum;   
+
+    }
+    
+};
