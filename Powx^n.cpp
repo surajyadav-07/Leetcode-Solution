61e15cc0964d313c05d058efe08b96feea63c702
Suraj Yadav <surajyadav63072@gmail.com>
2026-09-15 20:31:16 +0530

    Add Solution for Leetcode 50 - pow( x , n )
    Implement myPow function to calculate x raised to the power n using binary exponentiation.

---
+++ Powx^n.cpp
@@ -0,0 +1,22 @@
+// LeetCode 50. Pow(x, n)
+// https://leetcode.com/problems/powx-n/
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long binform = n;
+        if (n < 0) {
+            x = 1 / x;
+            binform = -binform;
+        }
+        double ans = 1;
+        while (binform > 0) {
+            if (binform % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            binform /= 2;
+        }
+        return ans;
+    }
+};
